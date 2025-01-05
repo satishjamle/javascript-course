91942245e5fd4309a2899d30e151907d976ba4e6
satishjamle <satishjamle513@gmail.com>
2025-01-06 01:02:01 +0530

    map.fillter.reduce

---
+++ Variables-1/Arrays-06/array-loop_03.js
@@ -0,0 +1,39 @@
+// Arrays-loops
+
+// forEach loops: call a function once for each array elements
+// koiii oprations krna hota h tho
+
+//note:- foreach ek loop h jo bar bar chlta h or har array element ke liye kuch krta h 
+
+// const a = [a, b, c, d, e];
+// a.forEach((value, index, Array) => {
+//   //function logic
+// });
+
+// let num = [3, 4, 6, 7, 8];
+// num.forEach((element) => {
+//   console.log(element * element);
+// });
+
+// Array.from
+// a ek object  h but (a.forEach) not a function
+//  function bnane ke liye( Array.from) use krte h
+
+// let name = 'satish'
+// let array = Array.from(name)
+// console.log(array)
+
+// for...of : array items ko access krne ka tarika h for of loops
+// let num = [1, 3, 4, 5, 6,"satish"]
+// for (let i of num) {
+//   console.log(i);
+// }
+
+// for...of : array items ko access krne ka tarika h for of loops
+// let item = [1, 3, 4, 5, 6] // key: value ke form me 
+// for (let i of item) {
+//   console.log(item);
+// //////   console.log(item[i]);
+// }
+
+
---
+++ Variables-1/Arrays-06/map_filter_reduce_04.js
@@ -0,0 +1,75 @@
+// Array map method ; array return krta h 
+// //  map : create a new array by ferforming  same oeratrion or each array element
+// let array = [45, 55, 43, 20];
+// let newarray = array.map((value) => {
+//     // console.log(value)
+//   return value + 1;
+// });
+// console.log(newarray);
+
+
+// let array = [45, 55, 43, 20];
+// let newarray = array.map((value,index ,array) => {
+//     console.log(value,index ,array)
+//     return value + 1;
+// });
+// console.log(newarray);
+
+// let array = [45, 55, 43, 20];
+// let newarray = array.map((value,index ,array) => {
+//     console.log(value,index ,array)
+//     return value + index; // index num add hoga value me
+// });
+// console.log(newarray);
+
+
+// array filter method :filter an array with calues that pass a test . create a new array
+// function jis jis ke liye true return krta h wo value opko dekhne ko mlegi new array me  
+
+//fillter array return krta h 
+
+// let array = [12,23,34,45,56]
+// let newarray = array.filter((a)=>{
+
+//     return a < 25
+
+// })
+// console.log(newarray)
+
+
+// filter new array ko modife nhi krte h 
+
+// let array = [12,23,34,45,56]
+// let newarray = array.filter((a)=>{
+
+//     return a < 25
+
+// })
+// console.log(newarray , array)
+
+
+// reduce: reduces an array to a single value
+// reduce ek value return krta h 
+
+// let array = [1,3,4,6,8,5]
+// let newarray = array.reduce((h1,h2)=>{
+//     return h1 + h2 ;   // 1+3 =4 , 4+4 = 8
+
+// })
+// console.log(newarray)
+
+// let array = [1,3,4,6,8,5]
+
+// let reduce_func = ((h1,h2)=>{
+//     return h1 + h2 ;  
+// })
+// let newarray = array.reduce(reduce_func)
+// console.log(newarray)
+
+
+// map  :  map  array return krta h 
+//fillter :fillter array return krta h 
+// reduce : reduce ek value return krta h 
+
+
+
